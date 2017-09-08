0cf6bd2e52b6d0f88172cf6219fece289ed8c31f
planglois7 <pnl@uoregon.edu>
2017-09-08 17:06:53 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+
+    function openNav() {
+        document.getElementById("mainNav").style.width = "320px";
+        document.getElementById("Main").style.marginLeft = "250px";
+    }
+
+    function closeNav() {
+        document.getElementById("mainNav").style.width = "0";
+        document.getElementById("Main").style.marginLeft = "0";
+    }
+
